3ed5a6dde85ce814fee11b86db164a27530f40ed
holmesgulnara <holmesgulnara@gmail.com>
2024-04-12 02:11:14 +0000

    Added Class5 files

---
+++ Class5/variables.tf
@@ -0,0 +1,33 @@
+variable region {
+  type = string
+  description = "Provide region"
+}
+
+variable vpc_cidr {
+  type = string
+  description = "provide vpc cidr block"
+}
+
+variable subnet1_cidr {
+  type = string
+  description = "provide subnet1 cidr block"
+}
+
+variable subnet2_cidr {
+  type = string
+  description = "provide subnet2 cidr block"
+}
+
+variable subnet3_cidr {
+  type = string
+  description = "provide subnet3 cidr block"
+}
+
+variable ip_on_launch {
+    type = bool  
+}
+
+variable instance_type {
+    type = string
+  
+}
